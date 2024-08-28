0d92ec1d0fa4ce78fa3a5fb19e051143b0892460
trent-8 <trenthek@gmail.com>
2024-08-27 23:15:55 -0500

    make note application alias permanent

--- configure.sh
+++ configure.sh
@@ -16,9 +16,8 @@ sudo cp .icons/default/index.theme ~/.icons/default
 sudo cp .Xresources ~/
 sudo cp .gtkrc-2.0 ~/
 sudo cp .config/gtk-3.0/settings.ini /etc/gtk-3.0
+sudo echo 'alias notes="python ~/trentos/notes.py"' > ~/.bashrc
 
 git config --global user.email "trenthek@gmail.com"
 git config --global user.name "trent-8"
 git config --global pull.rebase false
-
-alias notes="python ~/trentos/notes.py"
