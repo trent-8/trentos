4d22f9ba6830b36ca1a5a0f788685733dbcba0bb
trent-8 <trenthek@gmail.com>
2024-08-28 18:05:30 -0500

    mount onedrives at startup

--- startup.sh
+++ startup.sh
@@ -2,4 +2,8 @@
 ~/.screenlayout/apply-layout.sh &
 feh --bg-fill ~/trentos/dwm_background.svg
 dunst &
-picom
+picom &
+if [[ "$USER" == "trent" ]]; then
+    rclone mount --daemon school: ~/school
+    rclone mount --daemon personal: ~/personal
+fi
