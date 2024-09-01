0bdd9ecca01d3433ba4fddda071d1eef8b7008cc
trent-8 <trenthek@gmail.com>
2024-09-01 00:22:09 -0500

    update

--- install.sh
+++ install.sh
@@ -820,20 +820,9 @@ cd /home/$username
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-cd /home/$username
-git clone https://aur.archlinux.org/bluetuith.git
-cd bluetuith
-makepkg -si
-cd /home/$username
-git clone https://aur.archlinux.org/usbimager.git
-cd usbimager
-makepkg -si
-cd /home/$username
-git clone https://aur.archlinux.org/visual-studio-code-bin.git
-cd visual-studio-code-bin
-makepkg -si
-cd /home/$username
-git clone https://aur.archlinux.org/xcursor-breeze.git
-cd xcursor-breeze
-makepkg -si
+yay -S --noconfirm --needed\
+    bluetuith\
+    usbimager\
+    visual-studio-code-bin\
+    xcursor-breeze
 EOF
