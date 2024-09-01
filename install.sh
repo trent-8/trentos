21d14b61e3800d2edee21c4d86f40eb163de276d
trent-8 <trenthek@gmail.com>
2024-09-01 00:19:15 -0500

    update

--- install.sh
+++ install.sh
@@ -810,35 +810,29 @@ setfacl -R -m u:$username:rwx /home/$username
 EOF
 
 arch-chroot -u $username <<EOF
+set -x
 echo -ne "
 -------------------------------------------------------------------------
                     Make AUR Packages
 -------------------------------------------------------------------------
 "
-cd /home/$username/trentos
-echo $PWD
 cd /home/$username
-echo $PWD
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd /home/$username
-echo $PWD
 git clone https://aur.archlinux.org/bluetuith.git
 cd bluetuith
 makepkg -si
 cd /home/$username
-echo $PWD
 git clone https://aur.archlinux.org/usbimager.git
 cd usbimager
 makepkg -si
 cd /home/$username
-echo $PWD
 git clone https://aur.archlinux.org/visual-studio-code-bin.git
 cd visual-studio-code-bin
 makepkg -si
 cd /home/$username
-echo $PWD
 git clone https://aur.archlinux.org/xcursor-breeze.git
 cd xcursor-breeze
 makepkg -si
