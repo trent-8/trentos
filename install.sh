f45221a2bb25fb352ca8011ebddd647fb4f5fc0c
trent-8 <trenthek@gmail.com>
2024-08-31 22:26:21 -0500

    update

--- install.sh
+++ install.sh
@@ -282,27 +282,6 @@ userinfo () {
     export NAME_OF_MACHINE=$name_of_machine
 }
 
-makepackages () {
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si
-    cd /home/$username
-    cd bluetuith
-    makepkg -si
-    cd /home/$username
-    git clone https://aur.archlinux.org/usbimager.git
-    cd usbimager
-    makepkg -si
-    cd /home/$username
-    git clone https://aur.archlinux.org/visual-studio-code-bin.git
-    cd visual-studio-code-bin
-    makepkg -si
-    cd /home/$username
-    git clone https://aur.archlinux.org/xcursor-breeze.git
-    cd xcursor-breeze
-    makepkg -si
-}
-
 # Starting functions
 background_checks
 clear
