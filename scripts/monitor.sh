c924ec96bb84bcea9ccf4c68a6c38e2a0551f3f9
trent-8 <trenthek@gmail.com>
2024-09-12 00:15:45 -0500

    fix monitor script

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -3,8 +3,12 @@ intern=eDP-1
 extern1=HDMI-1
 extern2=DP-1
 
-if [ xrandr | grep "$extern1 disconnected" && xrandr | grep "$extern2 disconnected" ] ; then
-    xrandr --output "$extern1" --off --output "$extern2" --off --output "$intern" --auto
+if xrandr | grep "$extern1 disconnected" ; then
+    if xrandr | grep "$extern2 disconnected" ; then
+        xrandr --output "$extern1" --off --output "$extern2" --off --output "$intern" --auto
+    else
+        xrandr --output "$intern" --off --output "$extern1" --auto --output "$extern2" --auto
+    fi
 else
     xrandr --output "$intern" --off --output "$extern1" --auto --output "$extern2" --auto
 fi
