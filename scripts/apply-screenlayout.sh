158abdfe071b9d67fe11fc7f1a8bd86dd2aae6c8
trent-8 <trenthek@gmail.com>
2024-09-23 13:31:09 -0500

    remove everything to do with backgrounds and monitors

--- scripts/apply-screenlayout.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-if grep -q open /proc/acpi/button/lid/LID0/state; then
-    xrandr --output eDP-1 --auto --pos 320x1440 --rotate "$1" --output DP-1 --auto --pos 0x0
-else
-    xrandr --output eDP-1 --off --output DP-1 --auto --pos 0x0
-fi
-feh --bg-fill ~/trentos/bg.jpg
--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 # add items to autostart here
-~/trentos/scripts/apply-screenlayout.sh normal
 if [[ "$USER" == "trent" ]]; then
     rclone mount --daemon school: ~/school
     rclone mount --daemon personal: ~/personal
