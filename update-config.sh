e51dce848108a33dd0bcd7e486238036caf315b2
trent-8 <trenthek@gmail.com>
2024-11-13 09:22:19 -0600

    remove linternal display scaling

--- update-config.sh
+++ update-config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 script_dir=$( cd "$(dirname "${SH_SOURCE[0]}")" ; pwd -P )
 cd $script_dir
-cp -r .config/ Pictures/ .bash_profile $HOME/
+cp -r .config/ Pictures/ $HOME/
 sudo cp Hl.sh /usr/local/bin/Hl
 pkill waybar
 waybar </dev/null &>/dev/null &
