4aed1c9df94e1cbd39abe50c452eef58a4787787
trent-8 <trenthek@gmail.com>
2024-12-12 23:07:24 -0600

    fix zram setup

--- update-config.sh
+++ update-config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 script_dir=$(cd "$(dirname "$0")" && pwd -P)
-printf "[zram0]\nzram-size = ram * 2\ncompression-algorithm = zstd\n" > /mnt/etc/systemd/zram-generator.conf
+sudo printf "[zram0]\nzram-size = ram * 2\ncompression-algorithm = zstd\n" > /etc/systemd/zram-generator.conf
 cp -r "$script_dir/.config/" "$script_dir/Pictures/" "$script_dir/.zshrc" "$script_dir/.zprofile" "$HOME/"
 sudo cp -r $script_dir/scripts/* /usr/bin/
 systemctl --user restart waybar.service
