16581b411103e6d3c929b742fd9fad97682a9333
trent-8 <trenthek@gmail.com>
2024-12-15 21:45:46 -0600

    use sudo with pacman (obviously)

--- personal-config.sh
+++ personal-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-pacman -Sy
-pacman -S p7zip
+sudo pacman -Sy
+sudo pacman -S p7zip
 cd "$script_dir"
 7z x personal-dotfiles.zip
 mkdir personal-dotfiles
