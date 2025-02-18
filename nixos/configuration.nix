ff54159eb133e92f04b187f81204d88f7e0c75f1
trent-8 <trenthek@gmail.com>
2025-02-18 16:12:23 -0600

    use latest kernel, add pygame, add fractal images

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,6 +12,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "minipc";
   networking.networkmanager.enable = true;
@@ -167,6 +168,7 @@
         pypkgs.matplotlib
         pypkgs.numpy
         pypkgs.pandas
+        pypkgs.pygame
         pypkgs.scipy
         pypkgs.statistics
       ]))
@@ -237,6 +239,7 @@
         pypkgs.matplotlib
         pypkgs.numpy
         pypkgs.pandas
+        pypkgs.pygame
         pypkgs.scipy
         pypkgs.statistics
       ]))
--- nixos/user-units.nix
+++ nixos/user-units.nix
@@ -20,19 +20,22 @@
     };
   };
   ### uncomment for laptops with accelerometers
-  # systemd.user.services.orientation-manager = {
-  #   enable = true;
-  #   wantedBy = ["graphical-session.target"];
-  #   serviceConfig = {
-  #     ExecStart = "/usr/bin/orientation-manager";
-  #     Restart = "always";
-  #     RestartSec = 10;
-  #   };
-  #   unitConfig = {
-  #     After = "graphical-session-pre.target";
-  #     ConditionEnvironment = "WAYLAND_DISPLAY";
-  #     Description = "Screen Orientation Manager";
-  #     PartOf = "graphical-session.target";
-  #   };
-  # };
+  systemd.user.services.orientation-manager = {
+    enable = true;
+    wantedBy = ["graphical-session.target"];
+    serviceConfig = {
+      ExecStart =  let
+        python = pkgs.python3.withPackages (ps: with ps; [ requests ]);
+      in
+        "${python.interpreter} ${./executables/orientation-manager}";
+      Restart = "always";
+      RestartSec = 10;
+    };
+    unitConfig = {
+      After = "graphical-session-pre.target";
+      ConditionEnvironment = "WAYLAND_DISPLAY";
+      Description = "Screen Orientation Manager";
+      PartOf = "graphical-session.target";
+    };
+  };
 }
